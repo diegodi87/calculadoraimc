9f02d5291bfdc616a308747e142396c64a484659
diegodi87 <diegodi87@gmail.com>
2022-02-01 16:52:22 -0300

    calculadora imc

---
+++ imc/src/App.js
@@ -0,0 +1,27 @@
+import './app.css'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1> Calculadora IMC</h1>
+      <span> Vamos calcular seu IMC</span>
+
+      <div className="area-input">
+        <input
+          type="text"
+          placeholder="Peso em (kg) Ex: 90"
+        />
+
+        <input
+            type="text"
+            placeholder="Altura em (cm) Ex: 180"
+        />
+        <button>
+          Calcular
+        </button>
+        </div>
+
+      <h2> Seu imc foi 25, você esta abaixo do peso!</h2>
+      </div>
+      )
+};
